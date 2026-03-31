3e6c2a56a4a7e979e83b8b89f0fb9dda197361e8
Nick <nick.ruider@gmail.com>
2026-03-31 13:54:42 +0200

    Client package.json version upgrade

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,24 +1,5 @@
 ﻿const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
-const headerPluginRaw = require('eslint-plugin-header');
-
-const headerRule = headerPluginRaw.rules && headerPluginRaw.rules.header ? headerPluginRaw.rules.header : null;
-const headerPlugin = headerRule
-    ? {
-          ...headerPluginRaw,
-          rules: {
-              ...headerPluginRaw.rules,
-              header: {
-                  ...headerRule,
-                  meta: {
-                      ...(headerRule.meta || {}),
-                      // eslint-plugin-header does not define schema; ESLint 9 defaults to zero options.
-                      schema: [{}, {}, {}, {}]
-                  }
-              }
-          }
-      }
-    : headerPluginRaw;
 
 module.exports = [
     {
@@ -36,23 +17,6 @@ module.exports = [
     ...tseslint.configs.recommended,
     {
         files: ['**/*.ts', '**/*.tsx'],
-        plugins: {
-            header: headerPlugin
-        },
-        rules: {
-            'header/header': [
-                'error',
-                'block',
-                [
-                    '***************************************************************************',
-                    ' *',
-                    ' * Copyright © 2026 Nick Ruider. All rights reserved.',
-                    ' * This work is licensed under the terms of the MIT license.',
-                    ' * For a copy, see <https://opensource.org/licenses/MIT>.',
-                    ' *',
-                    ' ***************************************************************************'
-                ]
-            ]
-        }
+        rules: {}
     }
 ];
